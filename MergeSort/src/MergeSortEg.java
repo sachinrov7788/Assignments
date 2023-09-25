4521e37aea12c00e06f2b948e7b9cb292e111bbd
sachin778899 <20ad040@kpriet.ac.in>
2023-09-25 09:25:44 +0530

    Adding MergeSort

--- MergeSort/src/MergeSortEg.java
+++ MergeSort/src/MergeSortEg.java
@@ -1,31 +1,83 @@
 import java.util.Scanner;
 
-class MergeSortEg{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number of Elements : ");
-        int n = sc.nextInt();
+public class MergeSortEg {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
 
         int[] arr = new int[n];
-        System.out.println("Enter the Elements : ");
+        System.out.println("Enter the elements:");
 
-        for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        System.out.print("Original Array : ");
+        System.out.println("Original array:");
         printArray(arr);
 
-//        mergeSort(arr, 0, n-1);
-//
-//        System.out.println("Sorted Array : ");
-//        printArray(arr);
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+
+        scanner.close();
     }
 
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
 
-    public static void printArray(int[] arr){
-        for(int val : arr){
-            System.out.println(val+" ");
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
