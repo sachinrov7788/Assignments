234dd87187691991cd9df32c4384a9c0e5ab8536
sachin778899 <20ad040@kpriet.ac.in>
2023-09-25 12:36:25 +0530

    Adding LongestCollatzSequence

---
+++ LongestCollatzSequence/src/LongestCollatzSequenceEg.java
@@ -0,0 +1,37 @@
+import com.sun.source.tree.BreakTree;
+
+import java.util.Scanner;
+
+class LongestCollatzSequenceEg {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+
+        int num = sc.nextInt();
+        System.out.println(LongestCollatzsequence(num));
+    }
+
+    public static String LongestCollatzsequence(int num) {
+        if (num <= 0) {
+            return "Error";
+        }
+        String str = " ";
+        int counter = 0;
+
+        while (num != 4) {
+            counter++;
+            str += num + " ";
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = (num * 3) + 1;
+            }
+
+            if (counter >= 97 && num != 8) {
+                return "Does not Converg";
+            }
+        }
+        str = str + "4 2 1";
+        return str;
+    }
+}
