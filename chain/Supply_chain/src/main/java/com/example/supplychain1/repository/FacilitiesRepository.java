ac3c60e10bcb83ba04b937abeacd2d3ba370cc5f
Sachin <100345202+sachinrov7788@users.noreply.github.com>
2023-12-18 11:11:53 +0530

    Delete chain/Supply_chain directory

--- chain/Supply_chain/src/main/java/com/example/supplychain1/repository/FacilitiesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.supplychain1.repository;
-
-import com.example.supplychain1.model.Facilities;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FacilitiesRepository extends MongoRepository<Facilities,Long> {
-}
--- chain/Supply_chain/src/main/java/com/example/supplychain1/repository/StyleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.supplychain1.repository;
-
-import com.example.supplychain1.model.Style;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StyleRepository extends MongoRepository<Style, Long> {
-}
