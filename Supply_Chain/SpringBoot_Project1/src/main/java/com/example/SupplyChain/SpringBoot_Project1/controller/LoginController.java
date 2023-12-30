3d50d74a4d7c35e5ea3e0c8ac89a8ef169ed6abb
sachin778899 <20ad040@kpriet.ac.in>
2023-12-30 12:02:55 +0530

    Adding files

--- Supply_Chain/SpringBoot_Project1/src/main/java/com/example/SupplyChain/SpringBoot_Project1/controller/LoginController.java
+++ Supply_Chain/SpringBoot_Project1/src/main/java/com/example/SupplyChain/SpringBoot_Project1/controller/LoginController.java
@@ -1,34 +1,46 @@
 package com.example.SupplyChain.SpringBoot_Project1.controller;
 
+import javax.naming.NameNotFoundException;
+
 import com.example.SupplyChain.SpringBoot_Project1.service.impl.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 import jakarta.servlet.http.HttpServletRequest;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
-    LoginService service;
+    LoginService loginService;
 
     @PostMapping("/{username}/{pwd}")
     String login(@PathVariable String username, @PathVariable String pwd ){
-        return service.generateToken(username,pwd);
+        return loginService.generateToken(username);
     }
 
-    @GetMapping("/verify/{name}")
-    public String getMethodName(HttpServletRequest request, @PathVariable String name) {
-        return service.validateToken(request.getHeader("Authorization").split(" ",2)[1],name);
+    @GetMapping("/validate/{username}")
+    public String validate(@PathVariable String username,HttpServletRequest request) {
+        return loginService.validateToken(username,request);
     }
 
+    @ExceptionHandler(value=RuntimeException.class)
+    public ResponseEntity<?> runTimeException(){
+        return new ResponseEntity<>("Bad Request",HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value=IllegalArgumentException.class)
+    public ResponseEntity<?> illegalArgumentException(){
+        return new ResponseEntity<>("Illegal Argument",HttpStatus.BAD_REQUEST);
+    }
 }
--- Supply_Chain/SpringBoot_Project1/src/main/java/com/example/SupplyChain/SpringBoot_Project1/service/impl/LoginService.java
+++ Supply_Chain/SpringBoot_Project1/src/main/java/com/example/SupplyChain/SpringBoot_Project1/service/impl/LoginService.java
@@ -1,25 +1,27 @@
 package com.example.SupplyChain.SpringBoot_Project1.service.impl;
 
-import io.jsonwebtoken.*;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    public String generateToken(String username,String pwd){
-        Claims claims = (Claims) Jwts.claims().setSubject(username);
-        System.out.println("claims :"+claims);
+    public String generateToken(String name){
+        Claims claims = (Claims) Jwts.claims().setSubject(name);
         String token = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256,"*U(8hj908ns98daniasudfniawur97q2e7r2934892rnu213rn09217349782190348y12").compact();
-
-        System.out.println("token :"+token);
         return token;
     }
-    public String validateToken(String token,String name){
-        if(token.equals(generateToken(name, ""))){
-            return "Valid";
+    public String validateToken(String username, HttpServletRequest request){
+        String tokengot = request.getHeader("Authorization").substring(7);
+        System.out.println(tokengot);
+        String token=generateToken(username);
+        System.out.println(token);
+        if(token.equals(tokengot)){
+            return "Valid token";
         }
-        return "Invalid";
+        return "Invalid token";
     }
 }
